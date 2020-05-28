a0cd42ccc815d755b36afba07c7ecbcea24858f2
Patric Phinehas Raj <patricphinehas@gmail.com>
2020-05-28 17:04:07 +0530

    sohini'vocal

---
+++ vocalSplit.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+import librosa.output
+import os
+
+import librosa.display
+
+music_folder = os.environ["HOME"]
+music_path = music_path + "/Music"
+mp3file = "audio.mp3"
+mp3file = music_path + "/" + mp3file
+
+y, sr = librosa.load(mp3file, duration=120)
+
+
+# And compute the spectrogram magnitude and phase
+S_full, phase = librosa.magphase(librosa.stft(y))
+
+S_filter = librosa.decompose.nn_filter(S_full, aggregate=np.median, metric='cosine', width=int(librosa.time_to_frames(2, sr=sr)))
+
+S_filter = np.minimum(S_full, S_filter)
+
+margin_i, margin_v = 2, 10
+power = 2
+
+mask_i = librosa.util.softmask(S_filter, margin_i * (S_full - S_filter), power=power)
+
+mask_v = librosa.util.softmask(S_full - S_filter, margin_v * S_filter, power=power)
+
+S_foreground = mask_v * S_full
+S_background = mask_i * S_full
+
+
+new_y = librosa.istft(S_foreground*phase)
+music= librosa.istft(S_background*phase)
+librosa.output.write_wav("./voice.wav", new_y, sr)
+librosa.output.write_wav("./music.wav", music, sr)
+
+
+
